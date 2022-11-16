83a046040365c0566be8f65e2fb8f536c8c62b84
Maks <kiva_hill@mail.ru>
2022-11-16 11:33:28 +0300

    feat: Adding a slider

---
+++ src/static/script/script.js
@@ -0,0 +1,23 @@
+import Swiper, { Navigation, Pagination }  from "swiper";
+
+
+console.log(1);
+
+let swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    modules: [Navigation, Pagination],
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true
+    },
+
+    // Navigation arrows
+    navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+    },
+    observer: true,
+    observeParents: true
+});
