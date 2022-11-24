3fe3c165ae23d70efb36de0b17cee3f4fd1fce48
Maks <kiva_hill@mail.ru>
2022-11-24 17:23:33 +0300

    feat(assets) Chang the folder structure

--- wp-content/themes/rona/functions.php
+++ wp-content/themes/rona/functions.php
@@ -1,11 +1,11 @@
 <?php
     add_action( 'wp_enqueue_scripts', function(){
-        wp_enqueue_style( 'global', get_template_directory_uri() .  "/assets/dist/static/styles/global.css");
+        wp_enqueue_style( 'global', get_template_directory_uri() .  "/assets/static/styles/global.css");
         wp_enqueue_style( 'swiper-css', "https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css" );
-        wp_enqueue_style( 'style',  get_template_directory_uri() . "/assets/dist/static/styles/style.css");
+        wp_enqueue_style( 'style',  get_template_directory_uri() . "/assets/static/styles/style.css");
 
         wp_enqueue_script( 'swiper', "https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js", 'null', true);
-        wp_enqueue_script( 'main', get_template_directory_uri() . '/assets/dist/static/scripts/main.js',  array('swiper'), 'null', true);
+        wp_enqueue_script( 'main', get_template_directory_uri() . '/assets/static/scripts/main.js',  array('swiper'), 'null', true);
     });
 
     add_theme_support('post-thumbnails');
